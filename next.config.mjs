ed53d14eb94a4c670b52779537e81c1bda503055
jumagu <jmgg.gutierrez@gmail.com>
2024-05-23 22:47:13 -0500

    chore: Add remotePatterns for image sources in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "www.pngall.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
+  },
+};
 
 export default nextConfig;
